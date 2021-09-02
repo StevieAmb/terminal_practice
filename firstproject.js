090439e52014a7e20a2960a37b01065d1bde5b58
Stevie Ambroise <s.ambroise@protonmail.com>
2021-09-01 22:24:44 -0600

    Demonstrate Creation of Object Value of Variable Apt

--- firstproject.js
+++ firstproject.js
@@ -6,3 +6,12 @@ var numFloods = 1
 var whatToDo = "Call a Plumber!"
 
 var thingsDone = ["panicked", "cried", "studied"]
+
+var apartment = {
+  color: white,
+  decorations: ["plants", "wall hanging", "paintings"],
+  isCozy: true,
+  isCheap: false,
+  monthsLivedIn: 6,
+  iSay: "Welcome Home!"
+}
